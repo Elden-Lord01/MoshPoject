7168d2f182429483c4662010e6b23c119487d95e
Elden-Lord01 <levi.melton43@gmail.com>
2022-04-19 15:07:39 -0400

    artihmatic added.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,8 @@
 import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = {2,3,5,1,4};
-        Arrays.sort(numbers);
-        System.out.println(Arrays.toString(numbers));
+       int x = (10 + 3) * 2;
+        System.out.println(x);
+
     }
 }
