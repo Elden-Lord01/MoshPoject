a08ebfdf5f126b9d3a0dbfc1183f9fbfe629353a
Elden-Lord01 <levi.melton43@gmail.com>
2022-04-19 13:54:12 -0400

    Learned about string and functions.

--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 Added a date/ time program
+added primitive vs reference examples
+learned to string stuff together and different functions
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,9 +3,9 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        Point point1 = new Point(1, 1);
-        Point point2 = point1;
-        point1.x = 2;
-        System.out.println(point2);
+        String message = "Hello World" + "!!";
+
+        System.out.println(message.trim());
+        System.out.println(message);
     }
 }
